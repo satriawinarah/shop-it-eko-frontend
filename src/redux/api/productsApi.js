fa8c7b20b6cbec2479b97da5072860a2d5c389e2
Satria Winarah <satria.winarah@sawitpro.com>
2024-05-09 23:28:43 +0700

    using https

--- src/redux/api/productsApi.js
+++ src/redux/api/productsApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const productApi = createApi({
   reducerPath: "productApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://satria.win:4000/api/v1" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://satria.win:4000/api/v1" }),
   tagTypes: ["Product", "AdminProducts", "Reviews"],
   endpoints: (builder) => ({
     getProducts: builder.query({
